9ed7011a51e4ca387936add4a98a8557df64a6a7
Tiwarishubh <stshubhamtiwari001@gmail.com>
2024-07-26 13:51:53 -0600

    js-test-shubhamtiwari158

--- program1.js
+++ program1.js
@@ -1,8 +1,48 @@
 const getTotalIsles = function (grid) {
+  if (!grid || grid.length === 0 || grid[0].length === 0) {
+    return 0;
+  }
 
+  const rows = grid.length;
+  const cols = grid[0].length;
 
-  // write your code here
+  
+  function dfs(x, y) {
+    
+    if (x < 0 || x >= rows || y < 0 || y >= cols || grid[x][y] === 'W') {
+      return;
+    }
 
+    grid[x][y] = 'W';
+    
+    dfs(x + 1, y);
+    dfs(x - 1, y);
+    dfs(x, y + 1);
+    dfs(x, y - 1);
+  }
+
+  let islandCount = 0;
+
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < cols; j++) {
+      if (grid[i][j] === 'L') {  
+        islandCount++;
+        dfs(i, j);  
+      }
+    }
+  }
+
+  return islandCount;
 };
+
+
+const grid = [
+  ["L","L","W","W","W"],
+  ["L","L","W","W","W"],
+  ["W","W","L","W","W"],
+  ["W","W","W","L","L"],
+];
+
+console.log(getTotalIsles(grid));  
 
 module.exports = getTotalIsles;
